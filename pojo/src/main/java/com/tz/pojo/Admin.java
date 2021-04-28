4cffd1ae576ad9ac8eb3197f88d034ebd9dcf1f5
tz <1079032402@qq.com>
2021-04-28 18:59:32 +0800

    pojo

---
+++ pojo/src/main/java/com/tz/pojo/Admin.java
@@ -0,0 +1,100 @@
+package com.tz.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author TZ
+ * @since 2021-03-08
+ */
+@TableName("t_admin")
+public class Admin extends Model<Admin> {
+
+    private static final long serialVersionUID=1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String loginAcct;
+
+    private String userPswd;
+
+    private String userName;
+
+    private String email;
+
+    private String createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLoginAcct() {
+        return loginAcct;
+    }
+
+    public void setLoginAcct(String loginAcct) {
+        this.loginAcct = loginAcct;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUserPswd() {
+        return userPswd;
+    }
+
+    public void setUserPswd(String userPswd) {
+        this.userPswd = userPswd;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+        "id=" + id +
+        ", loginAcct=" + loginAcct +
+        ", userPswd=" + userPswd +
+        ", userName=" + userName +
+        ", email=" + email +
+        ", createTime=" + createTime +
+        "}";
+    }
+}
---
+++ pojo/src/main/java/com/tz/pojo/Role.java
@@ -0,0 +1,57 @@
+package com.tz.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author TZ
+ * @since 2021-04-20
+ */
+@TableName("t_role")
+public class Role extends Model<Role> {
+
+    private static final long serialVersionUID=1L;
+
+      @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String name;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+        "id=" + id +
+        ", name=" + name +
+        "}";
+    }
+}
