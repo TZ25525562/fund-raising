073fc5673806088d2bfb42e19223c178aefc995d
tz <1079032402@qq.com>
2021-04-28 18:59:43 +0800

    util

---
+++ util/src/main/java/com/tz/exception/AccessForbiddenException.java
@@ -0,0 +1,28 @@
+package com.tz.exception;
+
+/**
+ * 登录异常类
+ */
+public class AccessForbiddenException extends RuntimeException {
+    static final long serialVersionUID = 2L;
+
+    public AccessForbiddenException() {
+        super();
+    }
+
+    public AccessForbiddenException(String message) {
+        super(message);
+    }
+
+    public AccessForbiddenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AccessForbiddenException(Throwable cause) {
+        super(cause);
+    }
+
+    protected AccessForbiddenException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ util/src/main/java/com/tz/util/Constant.java
@@ -0,0 +1,20 @@
+package com.tz.util;
+
+/**
+ * 管理常量的类
+ */
+public class Constant {
+
+    //名称命名
+    public static final String EXCEPTION_NAME="exception";
+    public static final String LOGIN_ADMIN_NAME="loginAdmin";
+    public static final String PAGE_INFO_NAME="pageInfo";
+
+    //登录失败信息
+    public static final String ACCOUNT_IS_IN_USE="抱歉，您的账号已经被使用了";
+    public static final String FORBIDDEN_WITHOUT_ACCOUNT="抱歉，请登陆后再使用";
+    public static final String ACCOUNT_ACCESS_FAIL="抱歉，账号密码错误";
+    public static final String MASSAGE_STRING_INVALIDATE="字符串不合法，请不要传入空字符串";
+    public static final String MESSAGE_LOGIN_NOT_UNIQUE="系统错误：登陆账号不唯一";
+
+}
